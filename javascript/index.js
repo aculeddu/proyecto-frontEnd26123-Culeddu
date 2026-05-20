4f45f10395e11504dba2a44f8e0d37c925bd6a35
aculeddu <aculeddu@frba.utn.edu.ar>
2026-05-20 08:32:56 -0300

    Correcciones de la pre entrega y clase 9

---
+++ javascript/index.js
@@ -0,0 +1,27 @@
+const pulseraPerlas = {
+    nombre: "Pulsera de perlas",
+    precio: 10000,
+    stock: 1
+}
+
+const collarGamuza = {
+    nombre: "Collar de Gamuza",
+    precio: 30000,
+    stock: 4
+}
+
+const anilloColorido = {
+    nombre: "Anillos coloridos",
+    precio: 3000,
+    stock: 13
+}
+
+const arosRojos = {
+    nombre: "Aros rojos",
+    precio: 7000,
+    stock: 2
+}
+
+const catalogo = [pulseraPerlas, collarGamuza, anilloColorido, arosRojos]
+
+console.log(catalogo[1].nombre)
